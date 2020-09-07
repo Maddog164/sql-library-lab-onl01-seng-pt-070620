db39e8641ab2bbd77e942e075a2b5e8e4cf8778f
Denise Henderson <denise@henderson3.us>
2020-09-07 10:41:08 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -47,7 +47,7 @@ end
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT (*) as book_count
   FROM character_books
-  JOIN characters 
+  JOIN characters
   ON character_books.character_id = characters.id
   GROUP BY characters.name
   ORDER BY book_count DESC"
